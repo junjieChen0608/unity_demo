6a9f57fb60343ec8ce3db247b741971bbfbec21f
ruiqi <ruiqi.zhu@stonybrook.edu>
2020-09-18 10:25:08 +0800

    ruiqi level
    The enemy is jumping. The knight has to shoot him.

---
+++ Assets/BanditsJump.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+public class BanditsJump : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed = 4.0f;
+
+    int moveDir = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * moveDir * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bandit bandit = GetComponent<Bandit>();
+        if (bandit.getStatus() && collision.gameObject.name == "Wall")
+        {
+            moveDir = -1;
+        }
+        else if(bandit.getStatus() && collision.gameObject.name == "Ground")
+        {
+            moveDir = 0;
+        }else if (!bandit.getStatus())
+        {
+            moveDir *= -1;
+        }
+    }
+}
