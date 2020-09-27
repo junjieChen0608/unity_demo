8b91e3dd2622ec653b28b7776a215dcc011e01df
Monologue.№ <cancan@usc.edu>
2020-09-26 19:52:39 -0700

    Sonder Game

---
+++ Assets/Sonder/Scripts/DontDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    public GameObject[] objs;
+    void Awake ()
+    {
+        objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/Sonder/Scripts/Star.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    private Animator            m_animator;
+    private Rigidbody2D         m_body2d;
+    private bool                m_isAlive = false;
+
+    // Use this for initialization
+    void Start () {
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+ 
+    }
+
+    public void Live()
+    {
+        if(!m_isAlive)
+        {
+            Debug.Log("star is dying");
+            m_animator.SetTrigger("Live");
+            m_isAlive = true;
+        } else {
+            Debug.Log("star already live");
+        }
+    }
+}
