7b017db7ca923e509189639e85eb85cae7587594
Monologue.№ <cancan@usc.edu>
2020-10-01 01:08:40 -0700

    Create some dummy win transition. Next button clip will only take us to Level 2. Need to set some variable for sceneName in the future.
    Add a new BG and a moon sprite
    
    Organize the Sonder folder

--- Assets/Sonder/Scripts/Star.cs
+++ Assets/Sonder/Scripts/Star.cs
@@ -1,29 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Star : MonoBehaviour
 {
     private Animator            m_animator;
     private Rigidbody2D         m_body2d;
     private bool                m_isAlive = false;
+    private Animator            transitionAnim;
+    public string               sceneName;
 
     // Use this for initialization
-    void Start () {
+    public void Start () {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
+        transitionAnim = GetComponent<Animator>();
+        //sceneName = "Win";
  
     }
 
-    public void Live()
+    public void Live() 
     {
         if(!m_isAlive)
         {
             Debug.Log("star is dying");
             m_animator.SetTrigger("Live");
             m_isAlive = true;
+            StartCoroutine(Transition(sceneName));
         } else {
             Debug.Log("star already live");
         }
+    }
+    IEnumerator Transition(string sceneName) {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(7);
+        SceneManager.LoadScene(sceneName);
     }
 }
