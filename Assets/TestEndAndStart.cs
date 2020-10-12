100bc3f71d835c7f6682461af7a29ffb1705bd16
Yao <yaobian@usc.edu>
2020-10-12 16:29:36 -0700

    Change the recording function to a seperate script

---
+++ Assets/TestEndAndStart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class TestEndAndStart : MonoBehaviour
+{
+    private int countOfShoot = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        AnalyticsResult result = Analytics.CustomEvent("Test level starts");
+        Debug.Log("We are at " + scene.name);
+        Debug.Log("Analytic start results: " + result);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            countOfShoot++;
+            Debug.Log("we fired " + countOfShoot + " times hhh");
+        }
+    }
+
+    void OnDisable()
+    {
+        AnalyticsResult endResult = Analytics.CustomEvent("Test Star Dies");
+        Debug.Log("Analytic end results: " + endResult);
+        AnalyticsResult shootResult = Analytics.CustomEvent(
+            "Shoot for Test",
+            new Dictionary<string, object> {
+                    { "testLevel", countOfShoot }
+            });
+        Debug.Log("Analytic shoot results: " + shootResult);
+    }
+}
