e9f51d540eaa1c8ec376eff5303ac0cb36c283f6
Monologue.№ <cancan@usc.edu>
2020-10-14 17:03:10 -0700

    Modify PersistentManagerScript.cs, add some private instance

--- Assets/Sonder/Scripts/PersistentManagerScript.cs
+++ Assets/Sonder/Scripts/PersistentManagerScript.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class PersistentManagerScript : MonoBehaviour
 {
+    private static PersistentManagerScript _instance;
 
     public static PersistentManagerScript Instance 
     { 
-        get; private set; 
+        get { return _instance; }
+        // private set; 
     }
 
     public int LevelIdx;
     public int maxUnlockedIdx;
     public int[] LevelShots;
     public int[] bestLevelShots;
+    public int[] levelStarCnt;
 
     private void Awake()
     {
         
-        if (Instance == null)
+        if (_instance == null)
         {
-            Instance = this;
+            _instance = this;
             LevelShots = new int[10];
             bestLevelShots = new int[10];
+            levelStarCnt = new int[10];
             maxUnlockedIdx = 1;
             Debug.Log("This is initialized");
             DontDestroyOnLoad(gameObject);
