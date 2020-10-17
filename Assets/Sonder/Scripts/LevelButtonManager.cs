b3947bef39f25c823bf6ec202051124ade8113cd
ColeridgeGuo <yingxuan.guo9771@gmail.com>
2020-10-16 18:55:08 -0700

    disable (locked) buttons are now gray

---
+++ Assets/Sonder/Scripts/LevelButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButtonManager : MonoBehaviour
+{
+    public Button[] buttons;
+    
+    void Start()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i = 0; i < PersistentManagerScript.Instance.maxUnlockedIdx; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
