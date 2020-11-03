ce059147eb65a23888f49c4f5944d9842a782ce6
Monologue.№ <cancan@usc.edu>
2020-11-02 21:06:23 -0800

    Add a guide to Level_4

---
+++ Assets/Sonder/Scripts/GuideFade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideFade : MonoBehaviour
+{
+    private Animator fadeAnim;
+    private string TAG = "[GuideFade ]";
+
+    private void Start()
+    {
+        fadeAnim = GetComponent<Animator>();
+        Fade();
+    }
+
+    private void Fade() {
+        StartCoroutine(Transition());
+    }
+
+    IEnumerator Transition() {
+        yield return new WaitForSeconds(4);
+        fadeAnim.SetTrigger("Faded");     
+    }
+}
