5bbb145d667bc0606c11a5e7ccd50872b19459d9
Monologue.№ <cancan@usc.edu>
2020-11-06 19:59:44 -0800

    Modify return & mute buttons

---
+++ Assets/Sonder/Scripts/ReturnButtonAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnButtonAnimation : MonoBehaviour
+{
+    private string TAG = "[ReturnButtonAnimation ]";
+
+    public void onPointerEnter()
+    {
+        transform.localScale = new Vector3(1.05f, 1.05f, 1.05f);
+    }
+
+    public void onPointerExit()
+    {
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+
+    public void onPointerDown()
+    {
+        transform.localScale = new Vector3(0.92f, 0.92f, 0.92f);
+    }
+}
