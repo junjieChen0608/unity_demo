aebac3de4ea173d9f0426c001a7080d9a0572320
Monologue.№ <cancan@usc.edu>
2020-11-06 23:27:23 -0800

    Add some new music
    Add some best shot stars

---
+++ Assets/Sonder/Scripts/ShowBestFirstStar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBestFirstStar : MonoBehaviour
+{
+    public int LevelIdx;
+    private Animator m_animator;
+    private int currLevelIdx;
+   
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        currLevelIdx = PersistentManagerScript.Instance.LevelIdx;
+        
+        if (PersistentManagerScript.Instance.bestLevelShots[LevelIdx] < 1000)
+        {
+            m_animator.SetBool("getBestFirstStar", true);
+        }   
+    }
+}
---
+++ Assets/Sonder/Scripts/ShowBestThirdStar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBestThirdStar : MonoBehaviour
+{
+    public int LevelIdx;
+    private Animator m_animator;
+    private int currLevelIdx;
+   
+    void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+        currLevelIdx = PersistentManagerScript.Instance.LevelIdx;
+
+        if (PersistentManagerScript.Instance.bestLevelShots[LevelIdx] <= 3)
+        {
+            m_animator.SetBool("getBestThirdStar", true);
+        }    
+    }
+}
